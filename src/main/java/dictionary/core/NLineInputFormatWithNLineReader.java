518691c41db8bf165992ca94e61021a7b67e0daf
hadoop <patrick.k.hunter42@gmail.com>
2015-02-08 11:28:27 -0600

    Mavenize and get mapper working, with reading large splits, which is really slow btw

---
+++ src/main/java/dictionary/core/NLineInputFormatWithNLineReader.java
@@ -0,0 +1,16 @@
+package dictionary.core;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
+
+public class NLineInputFormatWithNLineReader extends NLineInputFormat {
+
+    @Override
+    public RecordReader<LongWritable, Text> createRecordReader(InputSplit split, TaskAttemptContext context) {
+        return new NLinesRecordReader();
+    }
+}
